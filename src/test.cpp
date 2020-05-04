f878c6ee6d824b238907cea7728a0d6f283ad5ae
agajews <agajews@gmail.com>
2020-05-03 22:19:00 -0400

    towards mini tensorflow

--- src/test.cpp
+++ src/test.cpp
@@ -11,97 +11,160 @@
 using namespace libsnark;
 using namespace std;
 
+typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
+typedef r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> KeyPair;
+typedef r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> Proof;
+
+struct ZKSystem {
+    protoboard<FieldT> pb;
+    std::vector<FieldElem> elems;
+
+    ZKSystem() {
+      default_r1cs_ppzksnark_pp::init_public_params();
+    }
+  
+    const KeyPair make_keypair() {
+      const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
+      return r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(constraint_system);
+    }
+
+    const Proof make_proof(KeyPair keypair) {
+        return r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
+    }
+
+    bool verify_proof(KeyPair keypair, Proof proof) {
+        return r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
+    }
+
+    FieldElem & def(std::string name) {
+        return register_elem(LeafFieldElem(name, this));
+    }
+
+    FieldElem & register_elem(FieldElem elem) {
+        int i = elems.size();
+        elems.push_back(elem);
+        return elems.at(i);
+    }
+
+    void allocate() {
+        int n_pub = 0;
+        for (FieldElem &elem : elems) {
+            if (elem.pub) {
+                n_pub += 1;
+                elem.pb_var.allocate(pb, elem.name);
+            }
+        }
+
+        for (FieldElem &elem : elems) {
+            if (!elem.pub) {
+                elem.pb_var.allocate(pb, elem.name);
+            }
+        }
+
+        pb.set_input_sizes(n_pub);
+    }
+
+}
+
+struct FieldElem {
+    ZKSystem &system;
+    pb_variable<FieldT> pb_var;
+    std::string name;
+    bool pub = false;
+
+    void make_public() {
+        pub = true;
+    }
+
+    friend FieldElem & operator+(const FieldElem &elem1, const FieldElem &elem2);
+    friend FieldElem & operator*(const FieldElem &elem1, const FieldElem &elem2);
+}
+
+struct LeafFieldElem : public FieldElem {
+    LeafFieldElem(std::string _name, ZKSystem &_system) : name(_name), system(_system);
+
+    int val;
+
+    void set(int x) {
+        val = x;
+    }
+
+    void eval() {
+        return val;
+        system.pb.val(pb_var) = val;
+    }
+};
+
+struct SumFieldElem : public FieldElem {
+    FieldElem &child_a, &child_b;
+
+    SumFieldElem(const FieldElem &elem1, const FieldElem &elem2, ZKSystem &_system) : child_a(elem1), child_b(elem2), system(_system) {
+        name = elem1.name + "+" + elem2.name;
+    }
+
+    int eval() {
+        int val = child_a.eval() + child_b.eval();
+        system.pb.val(pb_var) = val;
+        return val;
+    }
+}
+
+struct ProdFieldElem : public FieldElem {
+    FieldElem &child_a, &child_b;
+
+    ProdFieldElem(const FieldElem &elem1, const FieldElem &elem2, ZKSystem &_system) : child_a(elem1), child_b(elem2), system(_system) {
+        name = elem1.name + "*" + elem2.name;
+    }
+
+    int eval() {
+        int val = child_a.eval() * child_b.eval();
+        system.pb.val(pb_var) = val;
+        return val;
+    }
+}
+
+friend FieldElem & operator+(const FieldElem &elem1, const FieldElem &elem2) {
+    return elem1.system.register_elem(SumFieldElem(elem1, elem2, elem1.system));
+}
+
+friend FieldElem & operator*(const FieldElem &elem1, const FieldElem &elem2) {
+    return elem1.system.register_elem(ProdFieldElem(elem1, elem2, elem1.system));
+}
+
 int main()
 {
-  typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
 
   // Initialize the curve parameters
+  // Create zksystem
+  ZKSystem system;
 
-  default_r1cs_ppzksnark_pp::init_public_params();
-  
-  // Create protoboard
+  FieldElem &a1 = system.def("a1");
+  FieldElem &a0 = system.def("a0");
+  FieldElem &b1 = system.def("b1");
+  FieldElem &b0 = system.def("b0");
 
-  protoboard<FieldT> pb;
+  FieldElem &a1gtb1 = (1 - b1) * a1;
+  FieldElem &a0gtb0 = (1 - b0) * a0;
+  FieldElem &out = a1gtb1 + (1 - a1gtb1) * a0gtb0;
 
-  // Define variables
+  out.make_public();
+  system.allocate();
 
-  pb_variable<FieldT> a1, a0, b1, b0;
-  pb_variable<FieldT> a1gtb1, a0gtb0;
-  pb_variable<FieldT> not_a1gtb1_and_a0gtb0;
-  pb_variable<FieldT> agtb;
-  pb_variable<FieldT> out;
-
-  // Allocate variables to protoboard
-  // The strings (like "x") are only for debugging purposes
-  
-  out.allocate(pb, "out");
-
-  a1.allocate(pb, "a1");
-  a0.allocate(pb, "a2");
-  b1.allocate(pb, "b1");
-  b0.allocate(pb, "b0");
-  a1gtb1.allocate(pb, "a1gtb1");
-  a0gtb0.allocate(pb, "a0gtb0");
-  not_a1gtb1_and_a0gtb0.allocate(pb, "not_a1gtb1_and_a0gtb0");
-  agtb.allocate(pb, "agtb");
-
-  // This sets up the protoboard variables
-  // so that the first one (out) represents the public
-  // input and the rest is private input
-  pb.set_input_sizes(1);
-
-  // Add R1CS constraints to protoboard
-
-  // a1gtb1 = (1 - b1)a1
-  pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1 - b1, a1, a1gtb1));
-
-  // a0gtb0 = (1 - b0)a0
-  pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1 - b0, a0, a0gtb0));
-
-  // not_a1gtb1_and_a0gtb0 = (1 - a1gtb1)a0gtb0
-  pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1 - a1gtb1, a0gtb0, not_a1gtb1_and_a0gtb0));
-
-  // agtb = (a1gtb1 + not_a1gtb1_and_a0gtb0)1
-  pb.add_r1cs_constraint(r1cs_constraint<FieldT>(a1gtb1 + not_a1gtb1_and_a0gtb0, 1, agtb));
-
-  // out = (agtb)1
-  pb.add_r1cs_constraint(r1cs_constraint<FieldT>(agtb, 1, out));
-  
   // Add witness values
+  a1.val(1);
+  a0.val(0);
+  b1.val(0);
+  b0.val(1);
 
-  // inputs:
-  pb.val(a1) = 1;
-  pb.val(a0) = 0;
+  out.eval();
 
-  pb.val(b1) = 0;
-  pb.val(b0) = 1;
+  auto keypair = system.make_keypair();
+  auto proof = system.make_proof(keypair);
+  bool verified = system.verify_proof(keypair, proof);
 
-  // intermediate variables:
-  pb.val(a1gtb1) = 1;
-  pb.val(a0gtb0) = 0;
-  pb.val(not_a1gtb1_and_a0gtb0) = 0;
-  pb.val(agtb) = 1;
-
-  // output:
-  pb.val(out) = 1;
-
-  const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
-
-  const r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(constraint_system);
-
-  const r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
-
-  bool verified = r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
-
-  cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
-  cout << "Primary (public) input: " << pb.primary_input() << endl;
-  cout << "Auxiliary (private) input: " << pb.auxiliary_input() << endl;
+  cout << "Primary (public) input: " << system.pb.primary_input() << endl;
+  cout << "Auxiliary (private) input: " << system.pb.auxiliary_input() << endl;
   cout << "Verification status: " << verified << endl;
-
-  const r1cs_ppzksnark_verification_key<default_r1cs_ppzksnark_pp> vk = keypair.vk;
-
-  print_vk_to_file<default_r1cs_ppzksnark_pp>(vk, "../build/vk_data");
-  print_proof_to_file<default_r1cs_ppzksnark_pp>(proof, "../build/proof_data");
 
   return 0;
 }
